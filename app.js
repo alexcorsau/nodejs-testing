ff5bd026ae5b0a002c0972e84671096f612a3447
alexcorsau <77393961+alexcorsau@users.noreply.github.com>
2021-06-06 15:13:02 +0300

    finish example

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-function sayHello(name){
-    console.log("Hello " + name);
-}
-sayHello("Alex");
+const os=require("os");
+
+console.log(`Total memory: ${os.totalmem()/1048576} Gb`);
+console.log(`Free memory: ${os.freemem()/1048576} Gb`);
+
+console.log(os.userInfo());
